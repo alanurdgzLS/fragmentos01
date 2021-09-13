468b144c0796b8ff477e2c26b2fb1e7671112aa9
alanurdgz <nalan3947@gmail.com>
2021-09-13 16:13:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practicafragmentos/MainFragment.kt
@@ -0,0 +1,14 @@
+package com.example.practicafragmentos
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+
+}
